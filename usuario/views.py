b637093d9155a0546722f03e77a7f892f6c81ce5
agacy <antonio.lima13@estudante.ifto.edu.br>
2025-04-08 21:56:35 -0300

    versão 1.0

---
+++ usuario/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from .forms import UsuarioForm
+
+
+def criar_usuario(request):
+    form = UsuarioForm(request.POST)
+    if request.method == "POST":
+        form = UsuarioForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            return redirect('index')
+    return render(request, 'usuario/criar_usuario.html', {'form': form})
